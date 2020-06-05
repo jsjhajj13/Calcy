ae95333b20ad45b5c2b6b0bd45472cbf022c4ac8
Jagdev Singh Jhajj <jagdevsinghjhajj@Jagdevs-MacBook-Pro.local>
2020-06-05 18:19:54 -0400

    Project Complete

---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,62 @@
+//
+//  CalculatorLogic.swift
+//  Calcy
+//
+//  Created by Jagdev Singh Jhajj on 2020-06-05.
+//  Copyright © 2020 Jagdev Singh Jhajj. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorLogic{
+    
+    private var number: Double?
+    
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
+    
+    mutating func setNumber(_ number: Double){
+        self.number = number
+    }
+    
+    mutating func Calculate(symbol: String) -> Double?{
+        if let n = number{
+            if symbol == "+/-"{
+                return n * -1
+            }
+            else if symbol == "AC"{
+                return 0
+            }
+            else if symbol == "%"{
+                return n / 100
+            }
+            
+            else if symbol == "=" {
+                return performTwoNumCalculations(n2: n)
+            }
+            else{
+                intermediateCalculation = (n1: n, calcMethod: symbol)
+            }
+            
+        }
+        
+        return nil
+    }
+    
+    private func performTwoNumCalculations(n2: Double) -> Double?{
+        if let n1 = intermediateCalculation?.n1, let operation = intermediateCalculation?.calcMethod{
+            switch operation{
+            case "+":
+                return n1 + n2
+            case "÷":
+                return n1 / n2
+            case "x":
+                return n1 * n2
+            case "-":
+                return n1 - n2
+            default:
+                fatalError("Operation doesn't manage")
+            }
+        }
+        return nil
+    }
+}
