aae4f49baf31f5e893863a318eeb6cd0efb246c4
Jagdev Singh Jhajj <jagdevsinghjhajj@Jagdevs-MacBook-Pro.local>
2020-06-05 15:05:43 -0400

    Initial Setup

---
+++ Calcy/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Calcy
+//
+//  Created by Jagdev Singh Jhajj on 2020-06-05.
+//  Copyright © 2020 Jagdev Singh Jhajj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+    
+    }
+
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+    
+    }
+
+}
+
